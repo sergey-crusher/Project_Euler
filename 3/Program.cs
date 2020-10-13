6b794eaac1ae58153300f177a72ff2c0a22a7e44
sergey-crusher <sergey_crusher@gmail.com>
2020-10-13 23:27:05 +1100

    Initial commit

---
+++ 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long input = 13195; //result 5, 7, 13, 29
+            input = 600851475143;
+            var array = new HashSet<int>();
+            long side = input;
+            long div = 2;
+
+            while (side > 1)
+            {
+                while (side % div == 0)
+                {
+                    side /= div;
+                    Debug.Write(div + " ");
+                }
+                if (div == 2) div++;
+                //else if (div + 2 % 5 != 0 || div == 5) div += 2;
+                else div += 2;
+            }
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4
+{
+    class Program
+    {
+        static public string Revers (uint input)
+        {
+            char[] sReverse = input.ToString().ToCharArray();
+            Array.Reverse(sReverse);
+            string res = "";
+            foreach (char item in sReverse)
+            {
+                res += item;
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            uint result = 0;
+            List<uint> list = new List<uint>();
+            for (uint i = 999; i > 100; i--)
+            {
+                for (uint j = 999; j > 100; j--)
+                {
+                    result = i * j;
+                    if (result.ToString().Substring(0,3) == Revers(result).Substring(0, 3))
+                    {
+                        list.Add(result);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(list.Max());
+            Console.ReadKey();
+        }
+    }
+}
