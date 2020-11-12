b23da453be96d7747c41eb7f9f79d12159f494bf
sergey-crusher <sergey_crusher@gmail.com>
2020-11-13 10:58:08 +1100

    update

--- 3. Largest prime factor/Program.cs
+++ 3. Largest prime factor/Program.cs
@@ -13,29 +13,29 @@ namespace _3
      * What is the largest prime factor of the number 600851475143 ?
      */
     #endregion
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        long input = 13195; //result 5, 7, 13, 29
-        input = 600851475143;
-        var array = new HashSet<int>();
-        long side = input;
-        long div = 2;
-
-        while (side > 1)
+        static void Main(string[] args)
         {
-            while (side % div == 0)
+            long input = 13195; //result 5, 7, 13, 29
+            input = 600851475143;
+            var array = new HashSet<int>();
+            long side = input;
+            long div = 2;
+
+            while (side > 1)
             {
-                side /= div;
-                Console.Clear();
-                Console.Write(div + " ");
+                while (side % div == 0)
+                {
+                    side /= div;
+                    //Console.Clear();
+                    Console.Write(div + " ");
+                }
+                if (div == 2) div++;
+                //else if (div + 2 % 5 != 0 || div == 5) div += 2;
+                else div += 2;
             }
-            if (div == 2) div++;
-            //else if (div + 2 % 5 != 0 || div == 5) div += 2;
-            else div += 2;
+            Console.ReadKey();
         }
-        Console.ReadKey();
     }
-}
 }
---
+++ 7. 10001st prime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10_001;                                                                 //Количество простых чисел
+            int i = 1;                                                                      //Отчёт с 1, т.к её нет в массиве
+            int number = 3;                                                                 //Начальное число для цикла
+            List<int> numbers = new List<int>();                                            //Массив полученных простых чисел
+            numbers.Add(2);                                                                 //Добавляем двойку
+            bool check = true;                                                              //Для проверки - является ли простым
+
+            while (i < n)
+            {
+                check = true;
+                //Проверка - является ли число простым
+                foreach (int item in numbers)
+                {
+                    if (number % item == 0)
+                        check = false;
+                }
+                //Если является, добавляем его в массив
+                if (check)
+                {
+                    numbers.Add(number);
+                    Console.WriteLine($"{number} ");
+                    i++;
+                }
+                number += 2;                                                                //Пропускаем все чётные числа
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+#region
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//What is the 10 001st prime number?
+
+//Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-ое простое число - 13.
+//Какое число является 10001-ым простым числом?
+
+//104743
+#endregion
